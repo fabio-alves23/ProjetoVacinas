a2f5280f78d045cbc44531b1d9fc4327166448e8
Fabio Alves do Nascimento <fabio.nascimento@adagri.local>
2025-11-12 13:21:31 -0300

    feat: adiciona per_page no serviço e controller de listagem

--- app/Services/PetVacina/IndexPetVacinaService.php
+++ app/Services/PetVacina/IndexPetVacinaService.php
@@ -3,16 +3,13 @@
 namespace App\Services\PetVacina;
 
 use App\Models\PetVacina;
-use Illuminate\Http\Request;
 
 class IndexPetVacinaService
 {
-    public function run(Request $request, int $petId)
+    public function run(int $petId, int $perPage = 10)
     {
-        $per_page = $request->query('per_page', 10);
-        
         return PetVacina::with('vacina:id,nome')
             ->where('pet_id', $petId)
-            ->paginate($per_page);
+            ->paginate($perPage);
     }
 }
