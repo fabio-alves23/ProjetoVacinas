0cef3ac63af76349122877c06693ba4edd566eff
Fabio Alves do Nascimento <fabio.nascimento@adagri.local>
2025-12-05 16:05:44 -0300

    feat: ajustar CargosController com métodos de definição e gestão de permissões

--- app/Http/Controllers/CargosController.php
+++ app/Http/Controllers/CargosController.php
@@ -1,16 +1,21 @@
 <?php
 
-namespace App\Http\Controllers\API;
+namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Cargos\StoreCargosRequest;
 use App\Http\Requests\Cargos\UpdateCargosRequest;
+use App\Http\Requests\Cargos\SetPermissoesRequest;
 use App\Http\Resources\CargosResource;
 use App\Models\Cargos;
+
 use App\Services\Cargos\DeleteCargosService;
 use App\Services\Cargos\IndexCargosService;
 use App\Services\Cargos\StoreCargosService;
 use App\Services\Cargos\UpdateCargosService;
+use App\Services\Cargos\SetPermissoesService;
+
+use App\Services\Permissoes\CheckPermissoesService;
 
 class CargosController extends Controller
 {
@@ -26,15 +31,30 @@ class CargosController extends Controller
         return new CargosResource($cargo);
     }
 
-    public function update(UpdateCargosRequest $request, Cargos $cargos, UpdateCargosService $service)
+    public function update(UpdateCargosRequest $request, Cargos $cargo, UpdateCargosService $service)
     {
-        $cargos = $service->run($request->validated(), $cargos);
-        return new CargosResource($cargos);
+        $cargo = $service->run($request->validated(), $cargo);
+        return new CargosResource($cargo);
     }
 
     public function destroy($id, DeleteCargosService $service)
     {
         $service->run($id);
         return response()->json([], 204);
+    }
+
+    public function setPermissoes(
+        SetPermissoesRequest $request,
+        Cargos $cargo,
+        SetPermissoesService $service,
+        CheckPermissoesService $checkPermissoesService
+    ) {
+        // verifica se o usuário tem permissão
+        $checkPermissoesService->run('cargo.setPermissoes');
+
+        // aplica as permissões ao cargo
+        $cargo = $service->run($request->validated(), $cargo);
+
+        return response()->json($cargo);
     }
 }
